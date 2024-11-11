de324161d02868781286f5cc47b9515a323082b8
Jacob Tilly <dev@jacobtilly.com>
2024-11-11 17:08:15 +0100

    new functions for managing documents

--- config/laravel-docsign.php
+++ config/laravel-docsign.php
@@ -6,9 +6,17 @@ return [
      * You can obtain your own API key at https://docsign.se.
      */
     'api_key' => env('DOCSIGN_API_KEY', 'your-api-key-here'),
+    /*
+    * The callbacks configuration allows you to enable or disable the
+    * handling of callbacks from Inleed Docsign. If enabled, a callback route
+    * will be registered and the package will attempt to dispatch jobs for
+    * the following events:
+    * - Document complete
+    * - Party sign
+    */
     'callbacks' => [
         'enabled' => true,
-        'document_complete_job' => \JacobTilly\LaravelDocsign\Jobs\DocumentCompleteJob::class,
-        'party_sign_job' => \JacobTilly\LaravelDocsign\Jobs\PartySignJob::class,
+        'document_complete_job' => \JacobTilly\LaravelDocsign\Jobs\DocsignDocumentCompleteJob::class,
+        'party_sign_job' => \JacobTilly\LaravelDocsign\Jobs\DocsignPartySignJob::class,
     ],
 ];
